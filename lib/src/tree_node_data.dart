ae17f6a62e6b7f7da1874a0e628502cadefc347f
sdikmen <32899833+sdikmen@users.noreply.github.com>
2023-07-20 17:51:49 +0300

    Update tree_node_data.dart

--- lib/src/tree_node_data.dart
+++ lib/src/tree_node_data.dart
@@ -1,14 +1,8 @@
-import 'package:flutter/material.dart';
-
 class TreeNodeData {
   String title;
   bool expanded;
   bool checked;
   dynamic extra;
-  final Color? checkBoxCheckColor;
-  final MaterialStateProperty<Color>? checkBoxFillColor;
-  final ValueGetter<Color>? backgroundColor;
-  final List<Widget>? customActions;
   List<TreeNodeData> children;
 
   TreeNodeData({
@@ -17,17 +11,5 @@ class TreeNodeData {
     required this.checked,
     required this.children,
     this.extra,
-    this.checkBoxCheckColor,
-    this.checkBoxFillColor,
-    this.backgroundColor,
-    this.customActions,
   });
-
-  TreeNodeData.from(TreeNodeData other)
-      : this(title: other.title, expanded: other.expanded, checked: other.checked, extra: other.extra, children: other.children);
-
-  @override
-  String toString() {
-    return 'TreeNodeData{title: $title, expanded: $expanded, checked: $checked, extra: $extra, ${children.length} children}';
-  }
 }
